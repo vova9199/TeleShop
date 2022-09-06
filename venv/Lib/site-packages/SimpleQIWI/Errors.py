b8625166e3082eb783f7ddc0fad19098a8d45619
vova9199 <vova270999@gmail.com>
2022-09-07 00:34:56 +0300

    Upload on github the final version

---
+++ venv/Lib/site-packages/SimpleQIWI/Errors.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class QIWIAPIError(Exception):
+    pass
+
+
+class ArgumentError(Exception):
+    pass
+
+
+class InvalidTokenError(Exception):
+    pass
+
+
+class OverridingEx(Exception):
+    pass
